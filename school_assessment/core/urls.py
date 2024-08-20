db95c99450052f33b220ad175b657d228d5fe902
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-08-20 11:03:05 +0530

    authentication update

--- school_assessment/core/urls.py
+++ school_assessment/core/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('schools/', views.school_list, name='school_list'),
     path('students/', views.student_list, name='student_list'),
     path('summaries/', views.summary_list, name='summary_list'),
+    path('add_student/', views.add_student, name='add_student'),
+    path('add_summary/', views.add_summary, name='add_summary'),
 ]
--- school_assessment/core/views.py
+++ school_assessment/core/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import StudentForm, SummaryForm
 from .models import Summary, School, Student
 
+@login_required
 def home(request):
     return render(request, 'core/home.html')
 
+@login_required
 def school_list(request):
     schools = School.objects.all()
     return render(request, 'core/school_list.html', {'schools': schools})
 
+@login_required
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'core/student_list.html', {'students': students})
 
+@login_required
 def summary_list(request):
     summaries = Summary.objects.all()
     return render(request, 'core/summary_list.html', {'summaries': summaries})
+
+
+@login_required
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'core/add_student.html', {'form': form})
+
+@login_required
+def add_summary(request):
+    if request.method == 'POST':
+        form = SummaryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('summary_list')
+    else:
+        form = SummaryForm()
+    return render(request, 'core/add_summary.html', {'form': form})
