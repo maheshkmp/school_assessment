3ba78c46f114db247f9fccc02bf4b2c4816a783a
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-08-20 09:55:24 +0530

    Adding Models to Admin

--- school_assessment/core/admin.py
+++ school_assessment/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import School, Class, AssessmentAreas, Student, Answers, Awards, Subject, Summary
 
-# Register your models here.
+admin.site.register(School)
+admin.site.register(Class)
+admin.site.register(AssessmentAreas)
+admin.site.register(Student)
+admin.site.register(Answers)
+admin.site.register(Awards)
+admin.site.register(Subject)
+admin.site.register(Summary)
